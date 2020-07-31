74578a50d3b745f52011bcb656754c074687a230
niuben <niuben.wudi@qq.com>
2020-07-31 15:57:36 +0800

    补充fetch.js

--- utils/fetch.js
+++ utils/fetch.js
@@ -17,7 +17,15 @@ function olsfetch(api, path, params) {
   })
 }
 
-function olsfetchpost(api, path, params) {
+function olsfetchpost(api, path, params, log, showToast, loadingMsg) {
+  if (showToast) {
+    wx.showToast({
+      title: loadingMsg && loadingMsg != '' ? loadingMsg : '加载中',
+    })
+  }
+  if (log) {
+    console.log(log, '\n', path, "\n参数：\n", params)
+  }
   return new Promise((resolve, reject) => {
     wx.request({
       url: `${api}${path}`,
@@ -25,8 +33,86 @@ function olsfetchpost(api, path, params) {
       method: 'POST',
       // header: { 'Content-Type': 'json' },
       header: { 'content-type': 'application/x-www-form-urlencoded' },
-      success: resolve,
-      fail: reject
+      success (res) {
+        if (showToast) {
+          wx.hideLoading({
+            success: (res) => {},
+          })
+        }
+        if (log) {
+          console.log(log, "\n返回数据：\n", res)
+        }
+        let code = res.data.code*1
+        switch(code){
+          case 0: {
+            // 请求成功
+            resolve(res)
+            break
+          }
+          case 1: {
+            // 参数无效
+            resolve(res)
+            if (showToast) {
+              wx.showToast({
+                title: res.data.msg,
+                icon: 'none'
+              })
+            }
+            break
+          }
+          case 2: {
+            // token过期
+            let gid = wx.getStorageSync('gid')
+            wx.clearStorageSync()
+            wx.setStorageSync('gid', gid)
+            wx.reLaunch({
+              url: '../pages/first_page/first_page',
+            })
+            break
+          }
+          case 3: {
+            // 数据验证失败
+          }
+          case 4: {
+            // 无请求数据
+          }
+          case 5: {
+            resolve(res)
+            if (showToast) {
+              wx.showToast({
+                title: res.data.msg,
+                icon: 'none'
+              })
+            }
+            break
+          }
+          default: {
+            resolve(res)
+            if (showToast) {
+              wx.showToast({
+                title: '数据有误',
+                icon: 'none'
+              })
+            }
+          }
+        }
+      },
+      fail (res) {
+        if (showToast) {
+          wx.hideLoading({
+            success: (res) => {},
+          })
+        }
+        if (log) {
+          console.log(log, '请求失败：\n', res)
+        }
+        if (showToast){
+          wx.showToast({
+            title: '请求失败',
+            icon: "none"
+          })
+        }
+      }
     })
   })
 }
