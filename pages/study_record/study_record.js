e35c92ba2df9ebb3365193016366032430c6a478
niuben <niuben.wudi@qq.com>
2020-08-19 11:42:55 +0800

    日志视频播放 低版本升级提示

--- pages/study_record/study_record.js
+++ pages/study_record/study_record.js
@@ -607,9 +607,17 @@ Page({
       })
     } else {
       // 视频
-      wx.previewMedia({
-        sources: [{url: selectedFile.url, type: 'video'}],
-      })
+      if (wx.previewMedia) {
+        wx.previewMedia({
+          sources: [{url: selectedFile.url, type: 'video'}],
+        })
+      } else {
+        // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+        wx.showModal({
+          title: '提示',
+          content: '当前微信版本过低，无法播放该视频，请升级到最新微信版本后重试。'
+        })
+      }
     }
   },
 
